8f62918f84c5dcd8dd260edfc885a5f89f8622ed
Radee Al Mahmood <radee.mahmood@northsouth.edu>
2022-01-27 09:29:56 +0600

    Save Command Finished

---
+++ multiclipboard.py
@@ -0,0 +1,31 @@
+import sys
+import clipboard
+import json
+
+SAVED_DATA = "clipboard.json"
+
+def save_data(filepath, data):
+    with open(filepath, "w") as f:
+        json.dump(data, f)
+
+def load_items(filepath):
+    with open(filepath, "r") as f:
+        data = json.load(f)
+        return data
+
+if len(sys.argv) == 2:
+    command = sys.argv[1]
+    data = load_items(SAVED_DATA)
+
+    if command == "save":
+        key = input("Enter a Key: ")
+        data[key] = clipboard.paste()
+        save_items(SAVED_DATA, data)
+    elif command =="load":
+        print("load")
+    elif command == "list":
+        print("list")
+    else:
+        print("Unknown")
+else:
+    print("Please pass exacly one command.")
